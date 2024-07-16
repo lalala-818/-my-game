7ea96e01b314de6e60cde79a2c7f94e2920e5af7
syh <2737296900@qq.com>
2024-07-16 11:54:31 +0800

    用C#脚本添加了角色移动，并添加了移动动画

---
+++ Assets/script/move.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float moveSpeed = 5f; // 移动速度
+    Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // 创建移动向量
+        Vector3 movement = new Vector3(moveX, moveY, 0f);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+    }
+}
