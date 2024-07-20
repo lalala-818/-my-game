7e43404d5b949676ba26e5214e06adc1c4f6595b
syh <2737296900@qq.com>
2024-07-20 23:11:39 +0800

    添加了砍树掉落物

--- Assets/script/TreeHealth.cs
+++ Assets/script/TreeHealth.cs
@@ -6,6 +6,10 @@ public class TreeHealth : MonoBehaviour
 {
     public float maxHealth = 100.0f;
     private float currentHealth;
+    public GameObject woodprefab;
+    public int woodCount = 5;
+    public float woodforce = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +23,30 @@ public class TreeHealth : MonoBehaviour
         if (currentHealth <= 0)
         {
             Debug.Log("Tree Destroyed");
+            dropwood();
             Destroy(gameObject);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void dropwood()
     {
-        
+        for (int i = 0; i < woodCount; i++)
+        {
+            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+            GameObject wood = Instantiate(woodprefab, spawnPosition, Quaternion.identity);
+            Rigidbody2D rb2d = wood.GetComponent<Rigidbody2D>();
+            if (rb2d != null)
+            {
+                Vector2 forceDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(0.5f, 1f));
+                rb2d.AddForce(forceDirection * woodforce, ForceMode2D.Impulse);
+
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
